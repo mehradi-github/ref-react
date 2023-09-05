19e9dcec430d65a38eef74ebeecda0bf8e17fb41
Mehradi <mehradi.info@gmail.com>
2023-09-05 09:46:11 +0200

    Adding UserContextProvider

--- src/MyContexts/Contexts.js
+++ src/MyContexts/Contexts.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const UserContext = createContext({
   toggleUser: null,
@@ -7,3 +7,18 @@ export const UserContext = createContext({
     course: null,
   },
 });
+
+const user1 = { name: "user 1", course: "course 1" };
+const user2 = { name: "user 2", course: "course 2" };
+
+export const UserContextProvider = ({ childern }) => {
+  const [user, setUser] = useState(user1);
+
+  const toggleUser = user === user1 ? user2 : user1;
+
+  return (
+    <UserContext.Provider value={{ user, toggleUser }}>
+      {childern}
+    </UserContext.Provider>
+  );
+};
