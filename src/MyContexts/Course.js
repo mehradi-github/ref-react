a0967db90e1c08cee0b92466c515454b6e588289
Mehradi <mehradi.info@gmail.com>
2023-09-05 09:59:03 +0200

    Adding UseContext in sub-components

--- src/MyContexts/Course.js
+++ src/MyContexts/Course.js
@@ -1,4 +1,8 @@
+import { UserContext } from "./Contexts";
+import { useContext } from "react";
+
 const Course = () => {
-  return <></>;
+  const user = useContext(UserContext);
+  return <p>Your course is {user.Course}</p>;
 };
 export default Course;
--- src/MyContexts/Welcome.js
+++ src/MyContexts/Welcome.js
@@ -1,4 +1,8 @@
+import { useContext } from "react";
+import { UserContext } from "./Contexts";
+
 const Welcome = () => {
-  return <></>;
+  const user = useContext(UserContext);
+  return <h1>Hello {user.name}</h1>;
 };
 export default Welcome;
